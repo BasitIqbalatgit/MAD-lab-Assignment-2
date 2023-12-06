87241e8128594bdf0a5c834367dd15009bf87270
Fatima Aftab <129087237+fatimaaftab1388@users.noreply.github.com>
2023-12-06 13:20:30 +0500

    fatima

--- Lab/src/Components/Table.js
+++ Lab/src/Components/Table.js
@@ -11,21 +11,21 @@ const MyTable = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View>
+      <View style={styles.cardContainer}>
         <Card>
           <Card.Content>
             <DataTable>
               <DataTable.Header>
-                <DataTable.Title>Email</DataTable.Title>
-                <DataTable.Title>Name</DataTable.Title>
-                <DataTable.Title>Country</DataTable.Title>
+                <DataTable.Title style={styles.headerCell}>Email</DataTable.Title>
+                <DataTable.Title style={styles.headerCell}>Name</DataTable.Title>
+                <DataTable.Title style={styles.headerCell}>Country</DataTable.Title>
               </DataTable.Header>
 
               {data.map((item, index) => (
                 <DataTable.Row key={index}>
-                  <DataTable.Cell>{item.email}</DataTable.Cell>
-                  <DataTable.Cell>{item.name}</DataTable.Cell>
-                  <DataTable.Cell>{item.country}</DataTable.Cell>
+                  <DataTable.Cell style={styles.dataCell}>{item.email}</DataTable.Cell>
+                  <DataTable.Cell style={styles.dataCell}>{item.name}</DataTable.Cell>
+                  <DataTable.Cell style={styles.dataCell}>{item.country}</DataTable.Cell>
                 </DataTable.Row>
               ))}
             </DataTable>
@@ -40,6 +40,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 10,
+  },
+  cardContainer: {
+    marginVertical: 10,
+  },
+  headerCell: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  dataCell: {
+    fontSize: 14,
   },
 });
 
