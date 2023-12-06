3d2ad472aa6db98e83b7394c7c0fee4fb1d55fc1
Fatima Aftab <129087237+fatimaaftab1388@users.noreply.github.com>
2023-12-06 07:23:46 +0500

    fatima

--- Lab/App.js
+++ Lab/App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/Components/Login';
+import SignUp from './src/Components/Signup';
+import Home from "./src/Components/Home";
+import Homes from "./src/Components/Homes";
+import Calculator from "./src/Components/cal";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TaskForm from './src/Components/TaskForm';
+import NextPage from './src/Components/nextPage';
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer initialScreen = 'Homes'>
+      <Stack.Navigator>
+      <Stack.Screen name='Homes' component = {Homes} options={{headerShown: false}} />
+      <Stack.Screen name='Home' component = {Home} options={{headerShown: false}} />
+      <Stack.Screen name='nextPage' component = {NextPage} options={{headerShown: false}} />
+        <Stack.Screen name='Login' component={Login } options={{ headerShown: false }}/>
+        <Stack.Screen name='Signup' component={SignUp } options={{ headerShown: false }}/>
+        <Stack.Screen name='TaskForm' component={TaskForm} options={{headerShown: false}} />
+        <Stack.Screen name='Calculator' component={Calculator} options={{headerShown: false}} />
+      </Stack.Navigator>
+
+
+
+
+    </NavigationContainer>
+
+
+
   );
 }
 
